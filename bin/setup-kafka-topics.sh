25b08c8a4a21f1624cc48e2fa3032754642519df
salil <salil@xyz>
2018-12-09 22:30:32 +0000

    Updating script with correct topic name regex

--- bin/setup-kafka-topics.sh
+++ bin/setup-kafka-topics.sh
@@ -1,5 +1,5 @@
 # Delete Topics
-./kafka-topics.sh --zookeeper localhost:2181 --delete --topic kafka-test-101.*
+./kafka-topics.sh --zookeeper localhost:2181 --delete --topic kafka-.*
 
 # Create Topics
 ./kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic kafka-test-101
